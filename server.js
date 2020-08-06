075060eb47fb6f155b684c70ca2f696d18b4fc52
rishabh0211 <rishabh0211@gmail.com>
2020-08-06 15:45:58 +0530

    video chat

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuid } = require('uuid');
+const path = require('path');
+
+const staticPath = path.join(__dirname, './public');
+app.set('view engine', 'ejs');
+app.use(express.static(staticPath));
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuid()}`);
+});
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+    console.log(userId);
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId);
+    });
+  });
+});
+
+server.listen(3000);
