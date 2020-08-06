da696fe2328b129894f149b3d648b90c15f3a149
rishabh0211 <rishabh0211@gmail.com>
2020-08-06 19:01:35 +0530

    peer host

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const peer = new Peer(undefined, {
-  host: '/',
-  port: "3001"
+  host: 'videochatternet.herokuapp.com',
+  port: "443"
 });
 
 const myVideo = document.createElement('video');
